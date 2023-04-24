75ef28c0c1665eaa20c011d1538e1a2ee9d54e57
unknown <s.saurabh0205@gmail.com>
2023-04-24 21:07:26 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+"# MERN-Stack-Project-GUEST-HIVE-" 
+
+Go to "api/Config/default.json" and add your mongo db uri and a random json secret.
+
+Enter api folder and run backend using "nodemon index".
+
+Then enter client folder and run front end using "npm run dev".
+
+Let them  both run togethor on differrent ports and access the front end on port "5173".
---
+++ client/src/pages/LoginPage.jsx
@@ -0,0 +1,93 @@
+import { Link, Navigate } from "react-router-dom";
+import { useContext, useState } from "react";
+import axios from "axios";
+import { UserContext } from "../UserContext.jsx";
+import Index from './IndexPage.jsx';
+
+import { GoogleLogin } from 'react-google-login';
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [redirect, setRedirect] = useState(false);
+  const { setUser } = useContext(UserContext);
+
+  function onGoogleSuccess(response) {
+    // const id_token = response.tokenId;
+    // // Send the ID token to your backend for authentication
+    // const { data } = axios.post('/login', { id_token });
+    // // fetch('/google-authentication', {
+    // //   method: 'POST',
+    // //   headers: { 'Content-Type': 'application/json' },
+    // //   body: JSON.stringify({ id_token: id_token })
+    // // })
+    // // .then(response => response.json())
+    // // .then(data => {
+    // //   // Handle the response from your backend
+    // // });
+    // // setUser(data);
+    // alert('Login successful');
+    // setRedirect(true);
+    // setUser({ name: 'Saurabh' });
+    setRedirect(true);
+  }
+
+  // function onGoogleFailure(error) {
+  //   console.error(error);
+  // }
+
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+    try {
+      const { data } = await axios.post('/login', { email, password });
+      setUser(data);
+      alert('Login successful');
+      setRedirect(true);
+    } catch (e) {
+      alert('Login failed');
+    }
+  }
+
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
+
+  return (
+
+    // <div>
+
+    <div className="mt-4 grow flex items-center justify-around " >
+      <div className="mb-64">
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+          <input type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={ev => setEmail(ev.target.value)} />
+          <input type="password"
+            placeholder="password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)} />
+          <button className="primary">Login</button>
+          <div className="mt-6 mb-4 flex justify-center">
+            <GoogleLogin
+              clientId="123377236981-jqfgjot6f67f3k81ovm4giu6c5rdq1mb.apps.googleusercontent.com"
+              buttonText="Sign in with Google"
+              onSuccess={onGoogleSuccess}
+              // onFailure={onGoogleFailure}
+              cookiePolicy={'single_host_origin'}
+              className=""
+            />
+          </div>
+          <div className="text-center py-2 text-gray-500">
+            Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register now</Link>
+          </div>
+        </form>
+      </div >
+    </div >
+
+    // </div>
+
+
+  );
+}
